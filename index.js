9f77cd45403e4ef614618c80a014724c3b6d9725
MichelaKalowski <mi.ky90@hotmail.com>
2023-01-15 22:04:05 +0100

    aggiornamento

---
+++ index.js
@@ -0,0 +1,11 @@
+const navb = document.querySelector(".nav");
+const button= document.querySelector("button")
+window.onscroll = () => {
+    if (window.scrollY > 300) {
+        navb.classList.add("nav-scroll");
+        button.classList.add("button-scroll");
+    } else {
+        navb.classList.remove("nav-scroll");
+        button.classList.remove("button-scroll");
+    }
+}; 
